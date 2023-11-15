07de7bd3fe603db202a545997dca61c26da1f9c6
Markus Heinrichs <122888316+mheinri@users.noreply.github.com>
2023-11-15 14:05:39 +0100

    Update example_USB.m

--- Software/Control Software/MATLAB Code Examples/example_USB.m
+++ Software/Control Software/MATLAB Code Examples/example_USB.m
@@ -7,7 +7,7 @@ ris = serialport('COM7', 115200);
 pause(0.1);
 while ris.NumBytesAvailable > 0
     readline(ris);
-    pause(0.01);
+    pause(0.1);
 end
 
 %% Control examples
@@ -15,38 +15,37 @@ end
 % Set pattern
 writeline(ris, '!0XFFC00FFC00FFC00FFC00FFC00FFC00');
 % Get response
-pause(0.1);
-while ris.NumBytesAvailable > 0
-    response = readline(ris);
-    fprintf("Response from setting a pattern: %s\n", response);
-end
+response = readline(ris);
+fprintf("Response from setting a pattern: %s\n", response);
 
 % Read pattern
 writeline(ris, '?Pattern');
 % Get response
-pause(0.1);
-while ris.NumBytesAvailable > 0
-    currentPattern = readline(ris);
-    fprintf("Current pattern: %s\n", currentPattern);
-end
+currentPattern = readline(ris);
+fprintf("Current pattern: %s\n", currentPattern);
 
 % Read external supply voltage
 writeline(ris, '?Vext');
 % Get response
-pause(0.1);
-while ris.NumBytesAvailable > 0
-    externalVoltage = readline(ris);
-    fprintf("External supply voltage: %s\n", externalVoltage);
-end
+externalVoltage = readline(ris);
+fprintf("External supply voltage: %s\n", externalVoltage);
 
 % Set Static Pass Key
 writeline(ris, '!BT-Key=524953');
 % Get response
 % Wait long enough or check ris.NumBytesAvailable for becoming non-zero
 pause(5);
+response = readline(ris);
+fprintf("Response from setting a new Static Pass Key: %s\n", response);
+
+% Reset RIS
+writeline(ris, '!Reset');
+% Wait long enough or check ris.NumBytesAvailable for becoming non-zero
+pause(1);
 while ris.NumBytesAvailable > 0
     response = readline(ris);
-    fprintf("Response from setting a new Static Pass Key: %s\n", response);
+    fprintf("Response from resetting RIS: %s\n", response);
+    pause(0.1);
 end
 
 %% Deinitialization
